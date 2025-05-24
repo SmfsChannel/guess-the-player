7ef00ca35179ba01a2788c8ebba6c341129bfef4
TA-Salem <salemfh911@gmail.com>
2025-05-24 05:30:15 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>⚽ Guess the Player</h1>
+      <p>This is a placeholder for the game interface.</p>
+    </div>
+  )
+}
+
+export default App
